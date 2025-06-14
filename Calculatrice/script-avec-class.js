b45ed90523bfea1666413024d1076b2720356b6a
Lucas CAPPELLO <cappellolucas@icloud.com>
2025-06-14 23:12:37 +0200

    build test (calculatrice): script sans class

--- Calculatrice/script-avec-class.js
+++ Calculatrice/script-avec-class.js
@@ -3,48 +3,48 @@ class calculatrice {
     static valeur_A = '';
     static valeur_B = '';
     static operateur = '';
-    static resultat = '';
-    //FONCTION STATIQUE POUR CHIFFRES
+
+    //FONCTION POUR CHIFFRES
     static verifierValeurs(valeur) {
         if (this.operateur == '') {
             this.valeur_A += valeur;
             document.getElementById('affichage').textContent = this.valeur_A;
-        }
-        else if (this.operateur == '+' || '-' || '*' || '/') {
+        } else {
             this.valeur_B += valeur;
             document.getElementById('affichage').textContent = this.valeur_A + ' ' + this.operateur + ' ' + this.valeur_B;
-        }
-    }
-    //FONCTION STATQUE POUR OPERATEURS
+        };
+    };
+
+    //FONCTION POUR OPERATEURS
     static verifierOperateur(operateur) {
         this.operateur = operateur;
         document.getElementById('affichage').textContent = this.valeur_A + ' ' + this.operateur;
-    }
-    //FONCTION STATIQUE POUR RESULTAT
+    };
+
+    //FONCTION POUR RESULTAT
     static operation() {
+        let resultat = '';
         this.valeur_A = parseInt(this.valeur_A);
         this.valeur_B = parseInt(this.valeur_B);
         switch (this.operateur) {
             case "+":
-                this.resultat = this.valeur_A + this.valeur_B;
+                resultat = this.valeur_A + this.valeur_B;
                 break;
             case "-":
-                this.resultat = this.valeur_A - this.valeur_B;
+                resultat = this.valeur_A - this.valeur_B;
                 break;
             case "*":
-                this.resultat = this.valeur_A * this.valeur_B;
+                resultat = this.valeur_A * this.valeur_B;
                 break;
             case "/":
-                this.resultat = this.valeur_A / this.valeur_B;
+                resultat = this.valeur_A / this.valeur_B;
                 break;
-            default:
-                break;
-        }
+        };
         //AFFICHAGE DE LA REPONSE
-        document.getElementById('affichage').textContent = this.valeur_A + ' ' + this.operateur + ' ' + this.valeur_B + ' = ' + this.resultat;
-        //RÉINITIALISATION
+        document.getElementById('affichage').textContent = this.valeur_A + ' ' + this.operateur + ' ' + this.valeur_B + ' = ' + resultat;
+        //RESET
         this.valeur_A = '';
         this.valeur_B = '';
         this.operateur = '';
-    }
-}
+    };
+};
---
+++ Calculatrice/script-sans-class.js
@@ -0,0 +1,48 @@
+//PROPRIETES STATIQUES
+valeur_A = '';
+valeur_B = '';
+operateur = '';
+
+//FONCTION POUR CHIFFRES
+function verifierValeurs(valeur) {
+    if (operateur == '') {
+        this.valeur_A += valeur;
+        document.getElementById('affichage').textContent = valeur_A;
+    } else {
+        this.valeur_B += valeur;
+        document.getElementById('affichage').textContent = valeur_A + ' ' + operateur + ' ' + valeur_B;
+    };
+};
+
+//FONCTION POUR OPERATEURS
+function verifierOperateur(o) {
+    operateur = o;
+    document.getElementById('affichage').textContent = valeur_A + ' ' + operateur;
+};
+
+//FONCTION POUR RESULTAT
+function operation() {
+    let resultat = '';
+    valeur_A = parseInt(valeur_A);
+    valeur_B = parseInt(valeur_B);
+    switch (operateur) {
+        case "+":
+            resultat = valeur_A + valeur_B;
+            break;
+        case "-":
+            resultat = valeur_A - valeur_B;
+            break;
+        case "*":
+            resultat = valeur_A * valeur_B;
+            break;
+        case "/":
+            resultat = valeur_A / valeur_B;
+            break;
+    };
+    //AFFICHAGE DE LA REPONSE
+    document.getElementById('affichage').textContent = valeur_A + ' ' + operateur + ' ' + valeur_B + ' = ' + resultat;
+    //RESET
+    valeur_A = '';
+    valeur_B = '';
+    operateur = '';
+};

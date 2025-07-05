fc2c59504fc50284cbe80ae510347c26e775d699
Lucas CAPPELLO <cappellolucas@icloud.com>
2025-07-05 02:53:35 +0200

    test(thermomètre): ajout fonctionnalités darkmode + choix couleurs

--- Thermometre/script.js
+++ Thermometre/script.js
@@ -54,25 +54,27 @@ function affZone(dt) {
 // FONCTION CSS
 // FONCTION MODE DE COULEUR
 // FONCTION DARK
+const elMain = document.querySelector('main');
+const elInput = document.querySelector('input');
+const elAffichage = document.getElementById('id_affichage');
+const elBody = document.querySelector('body');
+const elBackGround = document.querySelector('video');
+
 const choix1 = document.getElementById('darkMode');
 choix1.addEventListener('change', () => {
     dark();
 });
 
-const elMain = document.querySelector('main');
-const elInput = document.querySelector('input');
-const elAffichage = document.getElementById('id_affichage');
-const elBody = document.querySelector('body');
-const elRange = document.getElementById('rangeIntensiter');
-const elColor = document.getElementById('choixCouleurs')
+function changeBody() {
+    elBackGround.setAttribute('src', "/testfolio/img/IA.appThermometre_videobg_fumee.mp4");
+}
 
 function dark() {
     elMain.classList.add('darkMain');
     elInput.classList.add('darkInput');
     elAffichage.classList.add('darkInput');
     elBody.classList.add('darkBody');
-    elRange.classList.add('disp');
-    elColor.classList.add('disp');
+    changeBody();
 }
 
 // FONCTION COULEURS
@@ -81,15 +83,46 @@ choix2.addEventListener('change', () => {
     couleurs();
 });
 
+function removeBody() {
+    elBackGround.setAttribute('src', "/testfolio/img/IA.appThermometre_videobg_ciel.mp4");
+}
+
 function couleurs() {
     elMain.classList.remove('darkMain');
     elInput.classList.remove('darkInput');
     elAffichage.classList.remove('darkInput');
     elBody.classList.remove('darkBody');
-    elRange.classList.remove('disp');
-    elColor.classList.remove('disp');
+    removeBody();
 };
 
-// FONCTION INTENSITER
 
 // FONCTION COULEUR DE FOND
+const input = document.getElementById('choixCouleurs');
+const affichageBordure = document.getElementById('id_affichage');
+const inputText = document.getElementById('localisation')
+
+function contreBlanc() {
+    const cible = document.querySelector('main');
+    const couleur = input.value;
+    if (couleur == '#ffffff') {
+        cible.style.backgroundColor = couleur;
+        cible.style.color = '#000000'
+        affichageBordure.style.borderColor = '#000000';
+        affichageBordure.style.color = '#000000';
+        inputText.style.borderBlockColor = '#000000';
+        inputText.style.color = '#000000';
+    } else if (couleur == '#000000') {
+        cible.style.backgroundColor = couleur;
+        cible.style.color = '#ffffff'
+        affichageBordure.style.borderColor = '#ffffff';
+        affichageBordure.style.color = '#ffffff';
+        inputText.style.borderBlockColor = '#ffffff';
+        inputText.style.color = '#ffffff';
+    } else {
+        cible.style.backgroundColor = couleur;
+    };
+};
+
+input.addEventListener('input', () => {
+    contreBlanc();
+});

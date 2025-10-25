530f6ffceb38ccc40ef5dfd37dcbaa7f22e2a3ed
Lucas CAPPELLO <cappellolucas@icloud.com>
2025-10-25 03:30:41 +0200

    test:(creperie app) ajustement et bonnes pratiques

--- CreperieApp/script-creperie.js
+++ CreperieApp/script-creperie.js
@@ -1,23 +1,23 @@
 // BOUTON MENU
-const back = document.getElementById('btnMenu');
+const back = document.getElementById('bouton_menu');
 back.addEventListener('click', () => {
     location.replace('http://localhost/testfolio/Menu/index.html');
 });
 
-// CLASSE CREPERIE
-class Creperie {
-
+// CLASSE CALCULATEUR DE CREPE
+class calculateur_de_crepe {
     constructor(id) {
         this.id = id;
         this.valeur = document.getElementById(id).value;
-
-        this.farine = 63;
-        this.lait = 0.1;
-        this.sucre = 0.5;
-        this.beurre = 13;
     };
 
-    addition() {
+    farine = 63;
+    lait = 0.1;
+    sucre = 0.5;
+    beurre = 13;
+
+    // FONCTION PRINCIPALE Résultat en fonction du nombre d'œuf
+    nombre_oeuf() {
         this.valeur = document.getElementById(this.id).value;
         let qttFarine = this.farine * this.valeur;
         let qttLait = this.lait + 0.12 * this.valeur;
@@ -28,68 +28,67 @@ class Creperie {
             document.getElementById('lait').innerText = `${qttLait.toFixed(1)} litre de lait`;
             document.getElementById('sucre').innerHTML = `${qttSucre.toFixed(1)} cuillère de sucre`;
             document.getElementById('beurre').innerHTML = `${qttBeurre.toFixed(1)}g de beurre fondu`;
-            this.aff();
+            this.affichage_resultat();
         } else {
-            document.getElementById('farine').innerText = 'You need some eggs !';
+            document.getElementById('farine').innerText = 'Il te faut au moins un œuf !';
             document.getElementById('lait').innerText = '';
             document.getElementById('sucre').innerText = '';
             document.getElementById('beurre').innerText = '';
-            this.aff();
+            this.affichage_resultat();
         };
     };
 
-    // BOUTONS ET FONCTIONS INCREMENTATION/DECREMENTATION
-    incrementation() {
+    // BOUTONS ET FONCTIONS bouton_incrementation/bouton_decrementation
+    bouton_incrementation() {
         let plus = document.getElementById(this.id).value;
         plus++;
         document.getElementById(this.id).value = plus;
-        this.addition();
+        this.nombre_oeuf();
     };
 
-    decrementation() {
+    bouton_decrementation() {
         if (this.valeur == 0) {
-            return
+            return;
         } else {
             let moins = document.getElementById(this.id).value;
             moins--;
             document.getElementById(this.id).value = moins;
-            this.addition();
+            this.nombre_oeuf();
         };
     };
 
-    // ANIMATION
-    aff() {
+    // ANIMATION CSS
+    affichage_resultat() {
         const id = document.getElementById('div_resultat');
-        id.classList.remove('aff');
-        id.classList.add('aff');
+        id.classList.remove('affichage_resultat');
+        id.classList.add('affichage_resultat');
     };
-
 };
 
 // INSTANCE
-const creperie = new Creperie('nombreOeuf');
+const instance = new calculateur_de_crepe('nombre_oeuf');
 
 // APPEL DE LA METHODE INCREMENTALE
-const incremente = document.getElementById('btnPlus');
+const incremente = document.getElementById('bouton_plus');
 incremente.addEventListener('click', () => {
-    creperie.incrementation();
+    instance.bouton_incrementation();
 });
 
 // APPEL DE LA METHODE DECREMENTALE
-const decremente = document.getElementById('btnMoins');
+const decremente = document.getElementById('bouton_moins');
 decremente.addEventListener('click', () => {
-    creperie.decrementation();
+    instance.bouton_decrementation();
 });
 
 // APPEL DE LA METHODE PRINCIPALE PAR LE BOUTON INGREDIENT
-const ingredients = document.getElementById('btnIngredients');
+const ingredients = document.getElementById('bouton_ingredients');
 ingredients.addEventListener('click', () => {
-    creperie.addition();
+    instance.nombre_oeuf();
 });
 
 // APPEL DE LA METHODE PRINCIPALE PAR LE BOUTON 'ENTER'
 addEventListener('keypress', (event) => {
     if (event.key == 'Enter') {
-        creperie.addition();
+        instance.nombre_oeuf();
     };
 });
